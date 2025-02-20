e3a9e12b90515f93740bbd788ccd75a463d70f51
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-20 01:36:34 +0100

    add form in html and function in js with refernces to the object

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// MILESTONE 1
+/* // MILESTONE 1
 
 // Funzione per calcolare il prezzo del biglietto
 function calcolaPrezzo() {
@@ -23,3 +23,27 @@ function calcolaPrezzo() {
 
 // Chiamata della funzione
 calcolaPrezzo();
+ */
+
+//####################################
+
+// MILESONE 2
+
+
+document.getElementById("ticketForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    let km = parseFloat(document.getElementById("km").value); //converte la stringa in numeri decimali
+    let eta = parseInt(document.getElementById("eta").value); //converte la stringa in numeri interi
+    
+    const prezzoKm = 0.21;
+    let prezzoTotale = km * prezzoKm;
+    
+    if (eta < 18) {
+        prezzoTotale *= 0.8; //20% di sconto
+    } else if (eta > 65) {
+        prezzoTotale *= 0.6; //40% di sconto
+    }
+    
+    document.getElementById("prezzo").innerText = `€${prezzoTotale.toFixed(2)}`;
+});
